d7ec0cfcb2930f7acdf7defb17fa0666283f40af
Vatsal Patel <f20212460@goa.bits-pilani.ac.in>
2024-01-30 22:24:38 +0530

    images opti

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
       backgroundImage: {
         "landing-image": "url('https://imgur.com/a/F6KvGQE.png')",
         hero: "url('https://i.imgur.com/BYu3zAD.png')",
-        "clients-image": "url('https://imgur.com/4wO9cQv.png')",
+        "clients-image": "url('https://i.imgur.com/eEVEIGs.png')",
         "client-01": "url('https://imgur.com/rRFilsZ')",
         "bgcc-logo": "url('https://i.imgur.com/I4dsioZ.png')",
-        "services-image": "url('https://imgur.com/PpWOGK4.png')",
-        "testimonials-image": "url('https://imgur.com/EhxwNPe.png')",
-        "events-image": "url('https://imgur.com/G3kjEWa.png')",
+        "services-image": "url('https://i.imgur.com/6y67Cza.png')",
+        "testimonials-image": "url('https://i.imgur.com/TEDOfJh.png')",
+        "events-image": "url('https://i.imgur.com/rc3m3mv.png')",
         "bg-loading":
           "url('https://cdn.dribbble.com/users/1991554/screenshots/7907346/media/4883405c5ebbb70d93ab4bf3be439db2.gif')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
