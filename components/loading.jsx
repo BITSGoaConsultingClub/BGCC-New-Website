50b5dce7bed5c2af72f477dcb4f1d7f8fe81b48d
Vatsal Patel <f20212460@goa.bits-pilani.ac.in>
2024-02-24 00:17:42 +0530

    Update loading.jsx

--- components/loading.jsx
+++ components/loading.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className=" h-[100vh] w-full overflow-hidden  flex flex-row justify-center">
+    <div className=" h-[100vh] w-full overflow-hidden  flex flex-row md:justify-center">
       <Image
         src={"https://i.imgur.com/U8atHTb.gif"}
         alt="jbdsjk"
-        width={700}
-        height={700}
+        width={500}
+        height={500}
         className=""
       />
     </div>
