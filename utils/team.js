4238b46bc9657b5bc5e6b90bd283922af0412be2
BITS Goa Consulting Club <141628178+BITSGoaConsultingClub@users.noreply.github.com>
2024-02-23 23:08:21 +0530

    Update team.js

--- utils/team.js
+++ utils/team.js
@@ -1,13 +1,6 @@
 //add the orignal team data to second index
 const team = [
   {
-    name: "Rupesh Yarlagadda",
-    designation: "President",
-    imgUrl: "https://imgur.com/pGcsJSp.png",
-    instaLink: "https://www.instagram.com/",
-    linkedinLink: "https://www.linkedin.com/in/rupesh-yarlagadda/",
-  },
-  {
     name: "Bhavya L Narayanan",
     designation: "Operations Director",
     imgUrl: "https://imgur.com/tBorfs7.png",
