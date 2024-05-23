da971f7d00e4f58c7b9c03ec491f74dc49300d3b
BITS Goa Consulting Club <141628178+BITSGoaConsultingClub@users.noreply.github.com>
2024-05-23 16:47:53 +0530

    Update TeamCard.jsx

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -15,7 +15,7 @@ export default function Team({ ind }) {
         <h2 className="text-2xl lg:text-3xl font-bold text-color-bgcc-yellow">
           {team[index].designation}
         </h2>
-        <p className="text-xl lg:text-2xl text-semibold text-white">
+        <p className="text-xl lg:text-xl text-semibold text-white">
           {team[index].name}
         </p>
         <div className="mt-2 flex space-x-4">
