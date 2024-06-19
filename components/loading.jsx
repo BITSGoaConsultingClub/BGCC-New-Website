7c3a4de173c7559aad2af943dc73ab0c900623ca
BITS Goa Consulting Club <141628178+BITSGoaConsultingClub@users.noreply.github.com>
2024-06-19 22:54:38 +0530

    Update loading.jsx

--- components/loading.jsx
+++ components/loading.jsx
@@ -5,8 +5,8 @@ export default function Loading() {
     <div className=" h-[100vh] w-full overflow-hidden  flex flex-row md:justify-center">
       <Image
         src={"https://i.imgur.com/EUzxAUw.gif"}
-        width={500}
-        height={500}
+        width={100}
+        height={100}
         className=""
       />
     </div>
